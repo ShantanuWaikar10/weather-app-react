a668516c5d9e9947f8072ab75ee6299958520f1d
ShantanuWaikar10 <89289274+ShantanuWaikar10@users.noreply.github.com>
2023-01-09 12:24:31 +0530

    first commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
+
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': process.env.X_RAPID_API_KEY_ID,
+		'X-RapidAPI-Key': 'b6c03ab9c4msh72a7aa4bcf7f64fp1f8a1djsn44f3d81da082',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -10,6 +11,6 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = process.env.WEATHER_API_KEY_ID
+export const WEATHER_API_KEY = '5fba667e2abc30105a31cbb0abdfb691'
 
 
